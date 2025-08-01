5a301d2a6d26e64be70a4a200a64fe59166847f6
asharma72 <ag.sharma23@hotmail.com>
2025-08-01 16:02:18 +0530

    exception handling with struct

--- logger/custom_logger.py
+++ logger/custom_logger.py
@@ -1,4 +1,32 @@
 
+# import logging
+# import os
+# from datetime import datetime
+
+# class CustomLogger:
+#     def __init__(self, log_dir ="logs"):
+#         self.log_dir = os.path.join(os.getcwd(), log_dir)
+#         os.makedirs(self.log_dir, exist_ok=True)
+
+#         log_file = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+#         log_file_path = os.path.join(self.log_dir, log_file) 
+
+#         logging.basicConfig(
+#         filename=log_file_path,
+#         format="[%(asctime)s] %(levelname)s %(name)s (line : %(lineno)d) - %(message)s",
+#         level=logging.INFO
+# )
+        
+#     def get_logger(self, name =__file__):
+#         return logging.getLogger(os.path.basename(name))
+    
+# if __name__ == "__main__":
+#     logger = CustomLogger()
+#     logger = logger.get_logger(__file__)
+#     logger.info("This is a custom logger message.")
+
+
+
 import logging
 import os
 from datetime import datetime
