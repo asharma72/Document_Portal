e9b742c4397d04f75d1e0fcb02e8bdc10a506a1e
asharma72 <ag.sharma23@hotmail.com>
2025-08-04 21:43:45 +0530

    code updated

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -5,11 +5,10 @@ from utils.config_loader import load_config
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
-#from langchain_openai import ChatOpenAIcls
+#from langchain_openai import ChatOpenAI
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 log = CustomLogger().get_logger(__name__)
-
 
 class ModelLoader:
     
